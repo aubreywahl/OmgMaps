f3a196a28007387aa39eba7c9dacab5035ae5716
Aubrey Wahl <aawahl@gmail.com>
2017-11-06 12:27:38 -0800

    initial component

---
+++ index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class Test extends Component {
+  render () {
+    return (
+      <View style={{flex: 1}}>
+        <Text>
+          Hey There
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default Test
